e5c557b1a5eb004588c2a3a518711b3ad79ea95f
Olha K <oliakozlovska02>
2023-03-05 17:19:38 +0200

    get result from numbers processing

--- Main.java
+++ Main.java
@@ -4,5 +4,10 @@ public class Main {
         a = (int) (20 + Math.random() * (30 - 20));
         b = (int) (40 + Math.random() * (50 - 40));
         System.out.println("a: " + a + "; b: " + b);
+        myNumbersProcessing(a, b);
+    }
+    static void myNumbersProcessing(int a, int b) {
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a * b = " + a * b);
     }
 }
