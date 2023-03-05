4e4f064f20e703693a3fd3044f9dcf566d4d6bcb
Olha K <oliakozlovska02>
2023-03-05 17:15:16 +0200

    set random value for a and b

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0, b = 0;
+        a = (int) (20 + Math.random() * (30 - 20));
+        b = (int) (40 + Math.random() * (50 - 40));
+        System.out.println("a: " + a + "; b: " + b);
+    }
+}
